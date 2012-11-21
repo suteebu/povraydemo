468e5767f8e50b8a99282b9a5b341060550a07d5
Stephen Elliott <stephee@amazon.com>
2012-11-21 21:52:21 +0000

    added new scripts

---
+++ povraydemo-monitor.rb
@@ -0,0 +1,103 @@
+# This demo shows how to use Amazon EC2 Spot Instances and Amazon SQS to
+# generate the movie images for a fly-by of a Julia Set Island.
+
+require 'rubygems'
+require 'yaml'
+require 'aws-sdk'
+require 'pp'
+
+# This file has my AWS access key and private key
+config_file = File.join(File.dirname(__FILE__), "config.yml")
+
+# IF CONFIG FILE DOESN'T EXIST, EXIT
+unless File.exist?(config_file)
+  puts <<END
+To run the samples, put your credentials in config.yml as follows:
+
+access_key_id: YOUR_ACCESS_KEY_ID
+secret_access_key: YOUR_SECRET_ACCESS_KEY
+
+END
+  exit 1
+end
+
+# LOAD CONFIG FILE
+config = YAML.load(File.read(config_file))
+
+# IF CONFIG FILE IS FORMATTED INCORRECTLY, EXIT
+unless config.kind_of?(Hash)
+  puts <<END
+config.yml is formatted incorrectly.  Please use the following format:
+
+access_key_id: YOUR_ACCESS_KEY_ID
+secret_access_key: YOUR_SECRET_ACCESS_KEY
+
+END
+  exit 1
+end
+
+# SET CONFIG SETTINGS
+AWS.config(config)
+
+# ACCESS JULIA ISLE QUEUE
+sqs = AWS::SQS.new
+queue = sqs.queues.create("JuliaIsleQueue")
+puts "Opened JuliaIsleQueue"
+pp sqs.queues.collect(&:url)
+
+# CREATING A BUCKET FOR THE FILES TO BE SAVED TO
+s3 = AWS::S3.new(:s3_endpoint => "s3-us-west-2.amazonaws.com")
+# create a bucket
+bucket_name = "stephee-povraydemo"
+b = s3.buckets[bucket_name]
+# create the bucket if it doesn't exist
+if not b.exists? then
+  b = s3.buckets.create(bucket_name)
+  puts "S3 bucket #{b.name} created in #{b.location_constraint}."
+else
+  puts "Using S3 bucket #{b.name} in #{b.location_constraint}."
+end
+
+# CHECK QUEUE LENGTH PERIODICALLY AND SEE WHAT SPOT INSTANCES HAVE CHECKED IN FOR DUTY
+
+count_empty = 0
+while count_empty < 3 do
+  msgs_left = queue.approximate_number_of_messages
+  if msgs_left == 0 then
+    count_empty += 1
+  end
+  puts "Approx number of msgs left: #{queue.approximate_number_of_messages}"
+  sleep(5)
+end
+
+puts "(press any key to download frames, render movie, and upload movie)"
+$stdin.getc
+
+# DOWNLOAD ALL MOVIE FRAMES
+# This should be its own file
+
+b.objects.each do |obj|
+  s3_filename = obj.key
+  if s3_filename.scan(/\w*.(\w*)/)[0][0] == "png"
+    puts "Downloading #{s3_filename}..."
+    File.open(s3_filename, 'w') do |file|
+      obj.read do |chunk|
+        file.write(chunk)
+      end
+    end
+  end
+end
+
+puts "Rendering movie..."
+movie_file_name = "JuliaIsleMovie.mp4"
+`ffmpeg -qscale 5 -r 24 -b 64k -i frame%03d.png #{movie_file_name}`
+
+puts "Uploading #{movie_file_name} to S3..."
+# Upload Movie file
+o = b.objects[movie_file_name]
+o.write(:file => movie_file_name)
+
+puts "To see the movie, visit this URL:"
+puts o.url_for(:read)
+
+puts "********** DONE! **********"
--- request-spot-instances.sh
+++ request-spot-instances.sh
@@ -5,4 +5,4 @@
 #echo "Getting spot price history:"
 #ec2-describe-spot-price-history -H --instance-type m1.xlarge --start-time $DTG_NOW --filter product-description='Linux/UNIX (Amazon VPC)'
 
-ec2-request-spot-instances ami-26c44c16 -p 0.10 --key stepheekey3 --group default --instance-type m1.xlarge --instance-count 100 --type one-time --user-data "/home/ubuntu/.rvm/rubies/ruby-1.9.3-p327/bin/ruby /home/ubuntu/povraydemo/povraydemo-client.rb"  --region us-west-2
+ec2-request-spot-instances ami-36e96106 -p 0.10 --region us-west-2 --key stepheekey3 --group default --instance-type m1.xlarge --instance-count 1 --type one-time --user-data "/home/ubuntu/.rvm/rubies/ruby-1.9.3-p327/bin/ruby /home/ubuntu/povraydemo/povraydemo-client.rb"
