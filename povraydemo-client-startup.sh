3d064f4bd5bb88c61110d548d2986cce5be94776
Elliott <stephee@u8c89a5ea16ce500f1c64.ant.amazon.com>
2012-11-22 03:16:41 +0000

    removed emailing at startup -- this is taken care of by spot notifications

--- povraydemo-client-startup.sh
+++ povraydemo-client-startup.sh
@@ -7,15 +7,15 @@ instance_id=$(wget -qO- instance-data/latest/meta-data/instance-id)
 public_hostname=$(wget -qO- instance-data/latest/meta-data/public-hostname)
 
 # Send status email
-/usr/sbin/sendmail -oi -t -f $EMAIL <<EOF
-From: $EMAIL
-To: $EMAIL
-Subject: Running POV-Ray Demo Client
+#/usr/sbin/sendmail -oi -t -f $EMAIL <<EOF
+#From: $EMAIL
+#To: $EMAIL
+#Subject: Running POV-Ray Demo Client
 
-This email was generated on the EC2 instance: $instance_id
-SSH into: ubuntu@$public_hostname
+#This email was generated on the EC2 instance: $instance_id
+#SSH into: ubuntu@$public_hostname
 
-EOF
+#EOF
 
 #cd /home/ubuntu/povraydemo
 /home/ubuntu/.rvm/rubies/ruby-1.9.3-p327/bin/ruby /home/ubuntu/povraydemo/povraydemo-client.rb -s true
