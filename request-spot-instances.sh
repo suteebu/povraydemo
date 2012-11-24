fa178ba52ab80a483709fea0e98f273d5b6d0bf5
Stephen Elliott <stephee@amazon.com>
2012-11-24 11:39:51 +0000

    requests 300 of different types

--- request-spot-instances.sh
+++ request-spot-instances.sh
@@ -5,4 +5,8 @@
 #echo "Getting spot price history:"
 #ec2-describe-spot-price-history -H --instance-type c1.xlarge --start-time $DTG_NOW --filter product-description='Linux/UNIX (Amazon VPC)'
 
-ec2-request-spot-instances ami-88ea62b8 -p 0.12 --region us-west-2 --key stepheekey3 --group default --instance-type c1.xlarge --instance-count 1 --type one-time --user-data-file povraydemo-client-startup.sh
+ec2-request-spot-instances ami-88ea62b8 -p 0.12 --region us-west-2 --key stepheekey3 --group default --instance-type c1.xlarge --instance-count 100 --type one-time --user-data-file povraydemo-client-startup.sh
+
+ec2-request-spot-instances ami-88ea62b8 -p 0.12 --region us-west-2 --key stepheekey3 --group default --instance-type c1.medium --instance-count 100 --type one-time --user-data-file povraydemo-client-startup.sh
+
+ec2-request-spot-instances ami-88ea62b8 -p 0.12 --region us-west-2 --key stepheekey3 --group default --instance-type m2.xlarge --instance-count 100 --type one-time --user-data-file povraydemo-client-startup.sh
